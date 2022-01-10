007c8f8b28694bf33ff98bf6b589f8036148f132
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-01-10 15:24:27 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,5 @@
+export const Form = () => {
+    return(
+        <h1>Página de FORM</h1>
+    );
+}
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,5 @@
+export const Listing = () => {
+    return(
+        <h1>Página de listagem</h1>
+    );
+};
