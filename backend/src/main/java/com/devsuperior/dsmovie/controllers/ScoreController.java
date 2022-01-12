89db35020300938e0119b07bf6d35fabb8783f8a
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-01-12 10:44:27 -0300

    Save score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
@@ -0,0 +1,21 @@
+package com.devsuperior.dsmovie.controllers;
+
+import com.devsuperior.dsmovie.DTO.MovieDTO;
+import com.devsuperior.dsmovie.DTO.ScoreDTO;
+import com.devsuperior.dsmovie.services.ScoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/scores")
+public class ScoreController {
+
+    @Autowired
+    private ScoreService scoreService;
+
+    @PutMapping
+    public MovieDTO saveScore(@RequestBody ScoreDTO scoreDTO) {
+        MovieDTO movieDTO = scoreService.saveScore(scoreDTO);
+        return movieDTO;
+    }
+}
