56b544f614f95721077943bb67ebfa994aa29492
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-01-14 13:33:09 -0300

    Show score

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -3,14 +3,61 @@ import { ReactComponent as StarHalf } from 'assets/images/star-half.svg';
 import { ReactComponent as StarEmpty } from 'assets/images/star-empty.svg';
 import './styles.css';
 
-export const MovieStars = () => {
+
+type Props = {
+    score: number;
+}
+
+type StarProps = {
+    fill: number;
+}
+
+function getFills(score: number) {
+
+    const fills = [0, 0, 0, 0, 0];
+  
+    const integerPart = Math.floor(score);
+  
+    for (let i = 0; i < integerPart; i++) {
+      fills[i] = 1;
+    }
+  
+    const diff = score - integerPart;
+    if (diff > 0) {
+      fills[integerPart] = 0.5;
+    }
+  
+    return fills;
+  }
+
+const Star = ({ fill }: StarProps) => {
+    switch(fill) {
+        case 0:
+            return <StarEmpty />
+        case 0.5:
+            return <StarHalf />
+        case 1:
+            return <StarFull />
+
+        default: 
+            return <StarHalf />
+    }
+}
+
+
+export const MovieStars = ({ score }: Props) => {
+
+    const fills = getFills(score);
+
+   
+
     return(
         <div className="dsmovie-stars-container">
-            <StarFull />
-            <StarFull />
-            <StarFull />
-            <StarHalf />
-            <StarEmpty />
+            {
+            fills.map((element, index ) => (
+                <Star fill={element} key={index}/>
+            ))
+            }
         </div>
     );
 }
